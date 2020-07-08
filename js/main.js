daf58de914b32019f1e75475777011806b7f55b1
Leandross86 <leandrobko@gmail.com>
2020-07-08 19:31:26 -0300

    Finalizado a parte lógica

--- js/main.js
+++ js/main.js
@@ -5,22 +5,46 @@ function cadastraVeiculo(e) {
     let placaCarro = document.getElementById('placaCarro').value;
     let time = new Date();
 
+    if(!modeloCarro && !placaCarro) {
+        alert('Por favor preencha os campos em branco!');
+        return false;
+    }
+
     carro = {
         modelo: modeloCarro,
         placa: placaCarro,
         hora: time.getHours(),
         minutos: time.getMinutes()
     }
+
+
     if (localStorage.getItem('patio2') === null) {
         let carros = []
         carros.push(carro);
         localStorage.setItem('patio2', JSON.stringify(carros));
     } else {
-        let carros = JSON.parse(localStorage.getItem('patio'));
+        let carros = JSON.parse(localStorage.getItem('patio2'));
         carros.push(carro);
         localStorage.setItem('patio2', JSON.stringify(carros));
     }
+
+    document.getElementById('formulario').reset();
+    mostraPatio();
     e.preventDefault();
+}
+
+function apagarVeiculo(placa){
+    let carros= JSON.parse(localStorage.getItem('patio2'));
+
+    for (let i = 0; i < carros.length; i++) {
+        if (carros[i].placa === placa){
+            carros.splice(i, 1);
+        }
+
+        localStorage.setItem('patio2', JSON.stringify(carros));
+    }
+
+    mostraPatio();
 }
 
 function mostraPatio () {
@@ -29,13 +53,16 @@ function mostraPatio () {
 
     carrosResultado.innerHTML = '';
 
-    for (let i = 0; i < carros.length; i++){
-        let modelo = carros[i].modelo;
-        let placa = carros[i].placa;
-        let hora = carros[i].hora;
-        let minutos = carros[i].minutos;
+    for (var i = 0; i < carros.length; i++){
+        var modelo = carros[i].modelo;
+        var placa = carros[i].placa;
+        var hora = carros[i].hora;
+        var minutos = carros[i].minutos;
         
-        let carrosResultado += <tr>
-                                    <td> + modelo + </td>
+        carrosResultado.innerHTML += '<tr><td>' + modelo +
+                               '</td><td>' + placa +
+                               '</td><td>' + hora + ' : ' + minutos +
+                               '</td><td><button class="btn btn-danger" onclick="apagarVeiculo(\''+ placa +'\')">Excluir</button></td>' +
+                               '</tr>';
     }
 }
